9232485f24a0b91e8b037106b07dde15845c8432
Ismail Koksal <ismail@koksal.fr>
2020-01-06 15:02:00 +0100

    Initial Commit

---
+++ NoteCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteCoreData
+//
+//  Created by Ismail Koksal on 06/01/2020.
+//  Copyright © 2020 Ismail Koksal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
